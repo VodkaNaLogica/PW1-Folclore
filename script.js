7594a3f0189a292a9eef3c634f1051b76150ffe5
P4 <p4bl0eduardo2011@gmail.com>
2026-08-08 14:53:18 -0300

    Adicionando o botão subir e ser clicavel

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-let i = 1;
+
 
 document.addEventListener('DOMContentLoaded', () => {
-    console.log(i)
+
 });
 
 const botaoLD = document.getElementById('BotaoP');
 
 botaoLD.addEventListener('click', () => {
-    if (i == 1) {i = 0} else {i = 1}
-    console.log(i)
-    botaoLD.classList.toggle('vermelho');
+    //window.location.href = "regioes/sudeste/indexSE.html";
 });
