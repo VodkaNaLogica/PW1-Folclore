95bd0f9b68be0d64217c685729138075067eeeff
P4 <p4bl0eduardo2011@gmail.com>
2026-08-09 18:01:49 -0300

    Um pequeno passo para os meus futuros conhecimentos em frontend, mas um grande passo para a conclusão do trabalho de PW.

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const topImg = document.getElementById('top');
 const bottomImg = document.getElementById('bottom');
+const direito = document.getElementById('direito');
+const esquerdo = document.getElementById('esquerdo');
+
+let escalaAtual = 1;
+let escalaDestino = 1;
+
+const SUAVIDADE = 0.5;
 
 function monitorarDistancia() {
     // Pega as coordenadas geométricas exatas e atuais de ambos os elementos na tela
@@ -9,9 +16,20 @@ function monitorarDistancia() {
     // Calcula a distância entre a base do #top e o topo do #bottom
     const distancia = rectBottom.top - rectTop.bottom;
     const distancia_medida = distancia + 400
+    const x = distancia_medida / 30
 
-    // Exibe o valor no console em tempo real (substitua pelo seu uso prático)
+    // Exibe o valor no console em tempo real (substitua pelo seu uso prático
     console.log(`Distância atual: ${distancia_medida.toFixed(2)}px`);
+
+    //escalaDestino = Math.max(0, Math.min(1, distancia_medida));
+    //escalaAtual += (escalaDestino - escalaAtual) * SUAVIDADE;
+
+    // 4. Aplica no elemento mantendo o translate se necessário
+    // Se não quiser manter o translate, use apenas: `scaleY(${fatorEscala})`
+    //esquerdo.style.transform = `scaleY(${escalaAtual})`
+    esquerdo.style.transform = `scaleY(${x})`
+    //direito.style.transform = `scaleY(${escalaAtual})`;
+    direito.style.transform = `scaleY(${x})`;
 
     // Mantém a função rodando em loop sincronizado com os frames da tela
     requestAnimationFrame(monitorarDistancia);
