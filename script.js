37a6dce4310f01ddda332c90056165010cbcc4a6
P4 <p4bl0eduardo2011@gmail.com>
2026-08-20 20:19:40 -0300

    margem consertada

--- script.js
+++ script.js
@@ -3,7 +3,11 @@ const bottomImg = document.getElementById('bottom');
 const direito = document.getElementById('direito');
 const esquerdo = document.getElementById('esquerdo');
 
-const svgButton = document.getElementById("landmarks-brazil2")
+const svgButton = document.getElementById("svg1")
+const svgPathA = document.querySelectorAll(".PathA")
+
+let estavel2 = 0
+let estavel = 0
 
 function monitorarDistancia() {
 
@@ -18,27 +22,28 @@ function monitorarDistancia() {
 
 	esquerdo.style.transform = `scaleY(${escalax})`
 	direito.style.transform = `scaleY(${escalax})`;
-	svgButton.style.transform = `transform: perspective(600px) rotateX(44deg) rotateY(0deg) rotateZ(0deg);`;
 
 	requestAnimationFrame(monitorarDistancia);
 }
 requestAnimationFrame(monitorarDistancia);
 
-function clicou() {
-	console.log("Clicou!")
-}
-svgButton.addEventListener("mousedown", () => {
-	svgButton.style.transform = `perspective(600px) rotateX(44deg) rotateY(0deg) rotateZ(0deg) translate(0px, -10px)`;
-})
+svgPathA.forEach(svgPathA => {
+	svgPathA.addEventListener("mousedown", () => {
+		svgButton.style.transform = `perspective(600px) rotateX(44deg) rotateY(0deg) rotateZ(0deg) translate(0px, -10px)`;
+		setTimeout(() => {
+			window.location.href = "regioes/sudeste/indexSE.html";
+		}, 300)
+	})
 
-svgButton.addEventListener("mouseup", () => {
-	svgButton.style.transform = `perspective(600px) rotateX(44deg) rotateY(0deg) rotateZ(0deg) translate(0px, -30px)`;
-})
+	svgPathA.addEventListener("mouseup",  () => {
+		svgButton.style.transform = `perspective(600px) rotateX(44deg) rotateY(0deg) rotateZ(0deg) translate(0px, -30px)`;
+	})
 
-svgButton.addEventListener("mouseenter", () => {
-	svgButton.style.transform = `perspective(600px) rotateX(44deg) rotateY(0deg) rotateZ(0deg) translate(0px, -30px)`;
-})
+	svgPathA.addEventListener("mouseenter", () => {
+		svgButton.style.transform = `perspective(600px) rotateX(44deg) rotateY(0deg) rotateZ(0deg) translate(0px, -30px)`;
+	})
 
-svgButton.addEventListener("mouseleave", () => {
-	svgButton.style.transform = `perspective(600px) rotateX(44deg) rotateY(0deg) rotateZ(0deg) translate(0px, 0px)`;
+	svgPathA.addEventListener("mouseleave",  () => {
+		svgButton.style.transform = `perspective(600px) rotateX(44deg) rotateY(0deg) rotateZ(0deg) translate(0px, 0px)`;
+	})
 })
